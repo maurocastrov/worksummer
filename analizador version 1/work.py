59f5b2c7e6af51ea6846affcb5b979a82d8153ac
maurocastrov <65107772+maurocastrov@users.noreply.github.com>
2021-10-04 19:08:09 +1000

    add new things

--- analizador version 1/work.py
+++ analizador version 1/work.py
@@ -1,40 +1,50 @@
+import sys
+import os
+#-*-coding: utf-8 -*-
 
 REL_BIN_OPS = ["<", "<=", ">", ">=", "==", "!=",":=","=",":"]
-ARITHM_BIN_OPS = ["+", '-', '*']
+ARITHM_BIN_OPS = ["+", '-',"*"]
 BOOL_BIN_OPS = ["&&", "||", "==", "!="]
 BOOL_UNARY_OPS = ["!"] 
 Final_line=["\n"]
-DELIM=[".",";",":",","]
+DELIM=[".",";",":",",",")"]
 ignore_comment = ["/","{","("]
+reservadas = ['array','downto','function','of','repeat','until','begin','else','goto','packed','set','var','case','end','if','procedure','then','while','const','file','label','program','to','with','do','for','nil','record','type'
+		]
 class lexema(object):
 	"""docstring for lexema"""
-	def __init__(self, sav , cha, lin ):
+	def __init__(self, sav , cha, lin, typ ):
 		self.save = sav
 		self.char=cha
-		self.line=lin		
-		
+		self.line=lin	
+		if sav == "":
+			self.end = True
+		else: 
+			self.end = False
+		self.type=typ
 class Lexer(object):
 
 	def __init__(self):
 
 		self.condition="S"#start
 		self.save=""
-		self.lecturaprimersimbolo = ""
+		self.lecturaprimersimbolo = " "
 		directorio = 'C:/Users/user/Documents/Documentos universidad 4 semestre/Compilador_PL0-master/analizador version 1/test/test.txt'
 		self.test =open(directorio,"r")
 		self.line=1 
 		self.char=0
+		self.type=self.condition
 	def siguientelexico(self):
 			
 		while True:
-
-
-
 			if self.condition=="S":
 				self.save=""
 				self.firstline=self.line
 				self.firstchar=self.char
-				if  self.lecturaprimersimbolo.isalpha() or self.lecturaprimersimbolo=="_":
+				if self.lecturaprimersimbolo =="":
+					break
+
+				elif self.lecturaprimersimbolo.isalpha() or self.lecturaprimersimbolo=="_":
 					self.save+=self.lecturaprimersimbolo
 					self.condition="kotoba"
 				
@@ -50,15 +60,22 @@ class Lexer(object):
 				elif self.lecturaprimersimbolo in BOOL_BIN_OPS:
 					self.save+=self.lecturaprimersimbolo
 					self.condition="booleanbin"	
+				elif self.lecturaprimersimbolo in DELIM:
+					self.save+=self.lecturaprimersimbolo
+					self.condition="delim"					
 				elif self.lecturaprimersimbolo==".":
 					self.save+=self.lecturaprimersimbolo
-					self.condition="ten"
+					self.condition="delim"
 				elif self.lecturaprimersimbolo =="{" :
 					self.condition="{"
 				elif self.lecturaprimersimbolo =="/" :
 					self.condition="/"
 				elif self.lecturaprimersimbolo =="(" :
 					self.condition="("
+				elif self.lecturaprimersimbolo=="(*" :
+					self.condition="(*"	
+				elif self.lecturaprimersimbolo in reservadas:
+					self.save+="reservacion"	
 	
 	
 			elif self.condition=="kotoba":
@@ -66,28 +83,29 @@ class Lexer(object):
 					self.save+=self.lecturaprimersimbolo
 					self.condition="kotoba"
 				else:
-					self.condition="finish"
+					# self.condition="finish"
 					break
 			elif self.condition=="operations":
 				if  self.lecturaprimersimbolo in ARITHM_BIN_OPS:
 					self.save+=self.lecturaprimersimbolo
 					self.condition="operations"
 				else:
-					self.condition="finish"
+					# self.condition="finish"
 					break
 			elif self.condition=="relations":
 				if  self.lecturaprimersimbolo in REL_BIN_OPS:
 					self.save+=self.lecturaprimersimbolo
 					self.condition="relations"
+
 				else:
-					self.condition="finish"
+					# self.condition="finish"
 					break
 			elif self.condition=="booleanbin":
 				if  self.lecturaprimersimbolo in BOOL_BIN_OPS:
 					self.save+=self.lecturaprimersimbolo
 					self.condition="booleanbin"
 				else:
-					self.condition="finish"
+					# self.condition="finish"
 					break	
 			elif self.condition=="bango":
 				if  self.lecturaprimersimbolo.isdigit():
@@ -99,7 +117,7 @@ class Lexer(object):
 				elif self.lecturaprimersimbolo.isalpha():
 					self.condition="error"																														
 				else:
-					self.condition="finish"
+					# self.condition="finish"
 					break	
 			elif self.condition=="error":
 				raise Exception('error')
@@ -108,23 +126,24 @@ class Lexer(object):
 				if  self.lecturaprimersimbolo==".":
 					presave=self.save[:-1]
 					self.save="."
+					condition="Numbers"
 					self.condition="delim"
-					return lexema(presave,self.firstchar, self.firstline)
+					return lexema(presave,self.firstchar, self.firstline, condition)
 				elif self.lecturaprimersimbolo.isdigit(): 	
 					self.save+=self.lecturaprimersimbolo
 					self.condition="float"																											
 				else:
-					self.condition="finish"
+					# self.condition="finish"
 					break		
 			elif self.condition=="delim":		
 				if  self.save=="..":
-					self.condition="finish"
+					# self.condition="finish"
 					break
 				elif self.lecturaprimersimbolo in DELIM :		
 					self.save+=self.lecturaprimersimbolo
 					self.condition="delim"																														
 				else:	
-					self.condition="finish"
+					# self.condition="finish"
 					break	
 			elif self.condition=="float":		
 				if  self.lecturaprimersimbolo.isdigit(): 	
@@ -134,44 +153,83 @@ class Lexer(object):
 					self.condition="error"
 					#raise Exception('error')																											
 				else:	
-					self.condition="finish"
+					# self.condition="finish"
 					break	
 			elif self.condition=="/":	
 				if self.lecturaprimersimbolo=="/"  :				
 					self.condition="//"
 				else:    
-					
 					self.condition="operations"
 					continue	
 			elif self.condition=="//":
 				if self.lecturaprimersimbolo in Final_line: 
 					self.condition='S'
-			elif self.condition=="{":		
+			elif self.condition=="(":
+			    			
 				if  self.lecturaprimersimbolo=="*":
-					self.condition="{*"
-					if 	self.lecturaprimersimbolo==")"
-						self.condition='S'
-					
+					self.condition="(*"
+				elif self.lecturaprimersimbolo==")":
+					self.save+=self.lecturaprimersimbolo
+					self.condition=='delim'
 				else: 	
-					self.condition=="operations" 																										
-			elif self.condition=="{*"
-						
+					self.save+=self.lecturaprimersimbolo
+					self.condition='delim'
+					
+																													
+			elif self.condition=="(*": 
+				if self.lecturaprimersimbolo=="*":
+					self.condition += "*"	
+			elif self.condition == "(**":
+				if self.lecturaprimersimbolo==")":
+						self.condition = 'S'
+						continue
+			elif self.condition=="{":
+				if self.lecturaprimersimbolo=="}":
+						self.condition= 'S'
+						continue	
+			elif self.condition=="reservacion":
+				if	self.lecturaprimersimbolo in reservadas:
+					self.save+=self.lecturaprimersimbolo					
+					self.condition="reservacion"
+				else:
+
+					# self.condition="finish"
+					break															
 			self.lecturaprimersimbolo=str(self.test.read(1))
-			self.char+=1	
+			self.char+=1
+				
 			if self.lecturaprimersimbolo in Final_line:
 				self.line+=1
 				self.char=0	
+		
+		conditn = self.condition
+		if self.save in REL_BIN_OPS:
+			conditn="binary operations"
+		elif conditn== "kotoba":
+			if self.save.upper() in reservadas:
+				conditn="Reservation words"
+			else:
+				conditn="words"	 
+
+
 		self.condition='S'
-			#return self.save
-		return lexema(self.save,self.firstchar,self.firstline)
+			#return self.saveb
+		return lexema(self.save,self.firstchar,self.firstline,conditn)
+		self.test.close()
 
 
-lexanalizer = Lexer()
-lex = lexanalizer.siguientelexico()
-while lex.save != "":
-	
-	print("line:"+ str(lex.line) + " char" + str(lex.char) +" lex:" + lex.save)
+if __name__ == '__main__':
+	lexanalizer = Lexer()
+	file2 = open("result\\test.txt",'w') 
 	lex = lexanalizer.siguientelexico()
+
+	massive=[]
+	while lex.save:
+		massive.append(lex)
+		file2.write("line:"+ str(lex.line) + " char" + str(lex.char) +" lex:" + lex.save+'\n'+lex.type)
+		print("line:"+ str(lex.line) + " char" + str(lex.char) +" lex:" + lex.save +" type: "+lex.type)
+		lex = lexanalizer.siguientelexico()
+
 
 		
 
